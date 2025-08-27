245f2b82098540df0a6c99a28eb9b73492a8f752
JdSeus <julianoseusdev@outlook.com>
2025-08-27 17:42:02 -0300

    Show All Posts Page

--- blogWeb/src/app/app.routes.ts
+++ blogWeb/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CreatePost } from './pages/create-post/create-post';
+import { ViewAll } from './pages/view-all/view-all';
 
 export const routes: Routes = [
-    { path: 'create-post', component: CreatePost }
+    { path: 'create-post', component: CreatePost },
+    { path: 'view-all', component: ViewAll }
 ];
---
+++ blogWeb/src/app/pages/view-all/view-all.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PostService } from 'app/service/post-service';
+import { MATERIAL_IMPORTS } from 'app/material.imports';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-view-all',
+    imports: [
+    ...MATERIAL_IMPORTS,
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './view-all.html',
+  styleUrl: './view-all.scss'
+})
+export class ViewAll {
+
+  allPosts:any;
+
+  constructor(
+    private postService: PostService, 
+    private snackBar: MatSnackBar
+  ){}
+
+  ngOnInit() {
+    this.getAllPosts();
+  }
+
+  getAllPosts() {
+    this.postService.getAllPosts().subscribe(res => {
+      console.log(res);
+      this.allPosts = res;
+    }, error => {
+      this.snackBar.open("Something went Wrong!", "Ok");
+    });
+  }
+
+}
