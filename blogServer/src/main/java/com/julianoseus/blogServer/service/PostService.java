a0fa1008c834e9db7172720b035cb8bf508f950a
JdSeus <julianoseusdev@outlook.com>
2025-08-27 15:33:10 -0300

    Post Blog

---
+++ blogServer/src/main/java/com/julianoseus/blogServer/service/PostService.java
@@ -0,0 +1,11 @@
+package com.julianoseus.blogServer.service;
+
+import com.julianoseus.blogServer.entity.Post;
+
+import java.util.Date;
+
+public interface PostService {
+
+    public Post savePost(Post post);
+
+}
---
+++ blogServer/src/main/java/com/julianoseus/blogServer/service/PostServiceImpl.java
@@ -0,0 +1,24 @@
+package com.julianoseus.blogServer.service;
+
+import com.julianoseus.blogServer.entity.Post;
+import com.julianoseus.blogServer.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post savePost(Post post) {
+        post.setLikeCount(0);
+        post.setViewCount(0);
+        post.setDate(new Date());
+
+        return postRepository.save(post);
+    }
+
+}
