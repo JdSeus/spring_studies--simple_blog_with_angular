0c63c4bf32053fbf3dd4547aabb8688e80a550ea
JdSeus <julianoseusdev@outlook.com>
2025-08-27 15:06:27 -0300

    Preparation of Angular App

--- blogWeb/src/app/app.ts
+++ blogWeb/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MATERIAL_IMPORTS } from './material.imports';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ...MATERIAL_IMPORTS, ReactiveFormsModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
