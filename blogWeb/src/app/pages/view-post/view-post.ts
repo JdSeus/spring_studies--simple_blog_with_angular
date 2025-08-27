a46143972eb00022b7f60ff1af03364b03b8b98c
JdSeus <julianoseusdev@outlook.com>
2025-08-27 20:00:30 -0300

    Adding Comments via Angular

--- blogWeb/src/app/pages/view-post/view-post.ts
+++ blogWeb/src/app/pages/view-post/view-post.ts
@@ -4,8 +4,9 @@ import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'app/service/post-service';
 import { MATERIAL_IMPORTS } from 'app/material.imports';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CommentService } from 'app/service/comment-service';
 
 @Component({
   selector: 'app-view-post',
@@ -23,17 +24,36 @@ export class ViewPost {
   postId: any;
   postData: any;
 
+  commentForm!: FormGroup;
+
   constructor(
     private postService: PostService,
     private activatedRoute: ActivatedRoute,
-    private matSnackBar: MatSnackBar
+    private matSnackBar: MatSnackBar,
+    private fb: FormBuilder,
+    private commentService: CommentService
   ){
     this.postId = this.activatedRoute.snapshot.params['id'];
+    this.commentForm = this.fb.group({
+      postedBy: [null, Validators.required],
+      content: [null, Validators.required],
+    });
   }
 
   ngOnInit() {
     console.log(this.postId);
     this.getPostById();
+  }
+
+  publishComment() {
+    const postedBy = this.commentForm.get('postedBy')?.value;
+    const content = this.commentForm.get('content')?.value;
+
+    this.commentService.createComment(this.postId, postedBy, content).subscribe(res => {
+      this.matSnackBar.open("Comment Published Succesfully!", "Ok");
+    }, error => {
+      this.matSnackBar.open("Something went Wrong!", "Ok");
+    });
   }
 
   getPostById() {
