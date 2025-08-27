d5d3f0b4af3a0ee13de60f7d05fa9be6c46521c2
JdSeus <julianoseusdev@outlook.com>
2025-08-27 16:50:08 -0300

    Work on Get All Posts

--- blogServer/src/main/java/com/julianoseus/blogServer/controller/PostController.java
+++ blogServer/src/main/java/com/julianoseus/blogServer/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/posts")
 @CrossOrigin(origins = "*")
@@ -20,6 +22,15 @@ public class PostController {
         try {
             Post createdPost = postService.savePost(post);
             return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(postService.getAllPosts());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
--- blogServer/src/main/java/com/julianoseus/blogServer/service/PostService.java
+++ blogServer/src/main/java/com/julianoseus/blogServer/service/PostService.java
@@ -3,9 +3,10 @@ package com.julianoseus.blogServer.service;
 import com.julianoseus.blogServer.entity.Post;
 
 import java.util.Date;
+import java.util.List;
 
 public interface PostService {
 
     public Post savePost(Post post);
-
+    public List<Post> getAllPosts();
 }
